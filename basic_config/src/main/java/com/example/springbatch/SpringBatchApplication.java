aa521dbca7ab9f21e3ae732b7f810ca875bed04c
moonhg <moonhg@AISELN-moonhg.Kumho.KumhoGroup.com>
2022-01-27 16:41:58 +0900

    불필요 삭제

--- basic_config/src/main/java/com/example/springbatch/SpringBatchApplication.java
+++ basic_config/src/main/java/com/example/springbatch/SpringBatchApplication.java
@@ -4,8 +4,6 @@ import org.springframework.batch.core.configuration.annotation.EnableBatchProces
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import jdk.jfr.Enabled;
-
 @EnableBatchProcessing
 @SpringBootApplication
 public class SpringBatchApplication {
